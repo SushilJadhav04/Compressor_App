9da92d0d9b6e9c11c541a9cc2061d510b8d71045
SushilJadhav04 <jadhavsushil052@gmail.com>
2025-04-05 21:34:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from compressor.image_compressor import compress_image
 from compressor.pdf_compressor import compress_pdf
+import os
 
 st.set_page_config(page_title="🗜️ File Compressor", layout="centered")
 st.title("Image & PDF Compressor")
@@ -18,14 +19,19 @@ if uploaded_file:
     if st.button("Compress"):
         with st.spinner("Compressing..."):
 
+            # Generate a new filename based on original
+            original_filename = uploaded_file.name
+            base_name, ext = os.path.splitext(original_filename)
+            new_filename = f"{base_name}-compressed{ext}"
+
             if file_type == "Image":
                 result = compress_image(uploaded_file, target_size_kb=target_size_kb)
                 final_size_kb = len(result.getvalue()) / 1024
                 st.success(f"Image compressed to {final_size_kb:.2f} KB")
-                st.download_button("Download Image", result, file_name="compressed.jpg")
+                st.download_button("Download Image", result, file_name=new_filename)
 
             elif file_type == "PDF":
                 result = compress_pdf(uploaded_file, target_size_kb=target_size_kb)
                 final_size_kb = len(result.getvalue()) / 1024
-                st.success(f"PDF compressed to {final_size_kb:.2f} KB(nearest possible based on content)")
-                st.download_button("Download PDF", result, file_name="compressed.pdf")
+                st.success(f"PDF compressed to {final_size_kb:.2f} KB (nearest possible based on content)")
+                st.download_button("Download PDF", result, file_name=new_filename)
