140530434e486eee78f29154fecfa1b1e3a47022
SushilJadhav04 <jadhavsushil052@gmail.com>
2025-04-05 19:58:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ st.title("📦 Image & PDF Compressor")
 uploaded_file = st.file_uploader("Upload an image or PDF", type=["jpg", "jpeg", "png", "pdf"])
 file_type = st.radio("Select File Type:", options=["Image", "PDF"])
 
-if uploaded_file and st.button("Compress"):
+if uploaded_file:
     if file_type == "Image":
-        result = compress_image(uploaded_file)
-        st.success("✅ Image compressed!")
-        st.download_button("Download Image", result, file_name="compressed_image.jpg")
-    elif file_type == "PDF":
-        result = compress_pdf(uploaded_file)
-        st.success("✅ PDF compressed!")
-        st.download_button("Download PDF", result, file_name="compressed_pdf.pdf")
+        quality = st.slider("Select Compression Quality", 10, 100, 60, step=10)
+
+    if st.button("Compress"):
+        if file_type == "Image":
+            result = compress_image(uploaded_file, quality=quality)
+            st.success("✅ Image compressed!")
+            st.download_button("Download Image", result, file_name="compressed_image.jpg")
+
+        elif file_type == "PDF":
+            result = compress_pdf(uploaded_file)
+            st.success("✅ PDF compressed!")
+            st.download_button("Download PDF", result, file_name="compressed_pdf.pdf")
